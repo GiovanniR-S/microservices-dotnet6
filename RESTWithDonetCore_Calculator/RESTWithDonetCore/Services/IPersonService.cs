ccedc02753fcea472d6c5a731efe5909d7d73f4f
Giovanni Silva <giovannirobertssilva@outlook.com>
2022-12-06 23:46:55 -0300

    Person service

---
+++ RESTWithDonetCore_Calculator/RESTWithDonetCore/Services/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using RESTWithDonetCore_Calculator.Model;
+using System.Collections.Generic;
+
+namespace RESTWithDonetCore_Calculator.Services
+{
+    public interface IPersonService
+    {
+
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
